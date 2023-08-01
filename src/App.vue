ef25fa2f8419113a0ffff4e4ef47365eaa542823
Hoku0306 <ciel0914@gmail.com>
2023-08-01 11:35:32 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,11 @@
 import NavBar from './components/NavBar.vue';
 import { RouterView } from 'vue-router';
 import FooterBar from './components/FooterBar.vue';
-import HomeVue from './views/HomeVue.vue';
 </script>
 
 <template>
   <NavBar></NavBar>
 
-  <HomeVue></HomeVue>
   <RouterView></RouterView>
 
   <FooterBar></FooterBar>
